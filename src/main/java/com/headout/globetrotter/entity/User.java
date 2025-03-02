5136daa326e4dd9358548688e5dfc9e524fa4b84
Jagat Bhan Singh <jagatbaghel8979@gmail.com>
2025-03-02 10:07:42 +0530

    Add User entity for managing user data and tracking answers in the game

---
+++ src/main/java/com/headout/globetrotter/entity/User.java
@@ -0,0 +1,24 @@
+package com.headout.globetrotter.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true, name = "username", nullable = false)
+    private String username;
+
+    private int correctAnswers;
+    private int wrongAnswers;
+}
