875bcdb1cdc7c99c745eff04ead9e72d9db7600a
Jagat Bhan Singh <jagatbaghel8979@gmail.com>
2025-03-02 10:08:34 +0530

    Add User repository for managing user data and retrieving users by username

---
+++ src/main/java/com/headout/globetrotter/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.headout.globetrotter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.headout.globetrotter.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
