f3cfb3faf76afe9fd5c575e0cde9968b1eef215d
Jagat Bhan Singh <jagatbaghel8979@gmail.com>
2025-03-02 10:37:11 +0530

    Downgrade Java version to 17 and clean up unused imports in Clue and Session entities

--- src/main/java/com/headout/globetrotter/entity/Clue.java
+++ src/main/java/com/headout/globetrotter/entity/Clue.java
@@ -5,14 +5,10 @@ import java.util.Collections;
 import java.util.List;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
--- src/main/java/com/headout/globetrotter/entity/Session.java
+++ src/main/java/com/headout/globetrotter/entity/Session.java
@@ -2,13 +2,8 @@ package com.headout.globetrotter.entity;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
