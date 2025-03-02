430803024f4d1e13a2fd26a2ec113ed7348b2fb2
Jagat Bhan Singh <jagatbaghel8979@gmail.com>
2025-03-02 10:07:02 +0530

    Add Clue entity for managing clues in the game

---
+++ src/main/java/com/headout/globetrotter/entity/Clue.java
@@ -0,0 +1,40 @@
+package com.headout.globetrotter.entity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Transactional
+@AllArgsConstructor
+@Table(name = "clue")
+public class Clue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "clue_text", nullable = false)
+    private List<String> clues;
+
+    public Clue(String clueText) {
+        this.clues = new ArrayList<>(Collections.singletonList(clueText));
+    }
+}
